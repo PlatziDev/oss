a9595fd88ba6422d0e04c6fa4f51caa91be5e351
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-01-12 16:27:39 -0500

    [add] custom server with in-memory cache

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const next = require('next');
+const LRUCache = require('lru-cache');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dir: '.', dev });
+const handle = app.getRequestHandler();
+
+const ssrCache = new LRUCache({
+  max: 100,
+  maxAge: 1000 * 60 * 60,
+});
+
+function renderAndCache(req, res, pagePath, queryParams) {
+  if (ssrCache.has(req.url)) {
+    console.log(`CACHE HIT: ${req.url}`);
+    res.send(ssrCache.get(req.url));
+    return;
+  }
+
+  app.renderToHTML(req, res, pagePath, queryParams)
+    .then((html) => {
+      console.log(`CACHE MISS: ${req.url}`);
+      ssrCache.set(req.url, html);
+
+      res.send(html);
+    })
+    .catch((err) => {
+      app.renderError(err, req, res, pagePath, queryParams);
+    });
+}
+
+app.prepare()
+.then(() => {
+  const server = express();
+
+  server.get('/', (req, res) => {
+    if (dev) return handle(req, res);
+    return renderAndCache(req, res, '/', req.query);
+  });
+
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(process.env.PORT || 3000, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${process.env.PORT || 3000}/`);
+  });
+})
+.catch(error => {
+  console.error(error);
+  process.exit(0);
+});
